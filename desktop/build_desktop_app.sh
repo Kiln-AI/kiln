8a222960cad81b0ac1eeb8a94bb7e5d442d52475
scosman <scosman@users.noreply.github.com>
2024-08-04 19:49:46 -0400

    Cleanup script

--- desktop/build_desktop_app.sh
+++ desktop/build_desktop_app.sh
@@ -8,4 +8,8 @@ cd ..
 
 # Builds the desktop app
 # TODO: use a spec instead of long winded command line
-pyinstaller --windowed --onedir --icon="../icon.png" --add-data "../taskbar.png:." --add-data "../../server/studio:./server/studio" -n fune --noconfirm --distpath=./desktop/build/dist --workpath=./desktop/build/work --specpath=./desktop/build --osx-bundle-identifier=net.scosman.fune --paths=. ./desktop/base.py
+pyinstaller --windowed --onedir --icon="../icon.png" \
+  --add-data "../taskbar.png:." --add-data "../../server/studio:./server/studio" \
+  -n fune --noconfirm --distpath=./desktop/build/dist --workpath=./desktop/build/work \
+  --specpath=./desktop/build --osx-bundle-identifier=net.scosman.fune \
+  --paths=. ./desktop/base.py
