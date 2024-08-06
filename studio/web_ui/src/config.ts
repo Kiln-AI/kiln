0387f8dbdfb5bc327491e5ec495c65ab31edb1a6
scosman <scosman@users.noreply.github.com>
2024-08-05 23:25:26 -0400

    Add basic web UI
    Note: this is based on CMSaasStarter project, which is available as MIT here https://github.com/CriticalMoments/CMSaasStarter. However, this copy is NOT being released open source/MIT.
    
    The CM logo is owned by Chesterfield Labs, and it's inclusion here isn't giving away any rights.

---
+++ studio/web_ui/src/config.ts
@@ -0,0 +1,3 @@
+export const WebsiteName: string = "Fune"
+export const WebsiteBaseUrl: string = "https://saasstarter.work"
+export const WebsiteDescription: string = "Build ML Products with Fune Studio"
