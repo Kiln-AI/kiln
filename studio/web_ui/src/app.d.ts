244446ef83c28e2cdc3d5806180c1df41e4e917b
scosman <scosman@users.noreply.github.com>
2024-08-05 23:32:38 -0400

    Try workflow fix

--- studio/web_ui/src/app.d.ts
+++ studio/web_ui/src/app.d.ts
@@ -1,5 +1,3 @@
-import { SupabaseClient, Session } from "@supabase/supabase-js"
-import { Database } from "./DatabaseDefinitions"
 
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
