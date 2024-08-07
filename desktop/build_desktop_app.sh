36354bff466c550a1e9ad8b886333084953a53a4
scosman <scosman@users.noreply.github.com>
2024-08-07 16:27:10 -0400

    Update windows build to onefile

--- desktop/build_desktop_app.sh
+++ desktop/build_desktop_app.sh
@@ -11,9 +11,17 @@ cd studio/web_ui
 npm run build
 cd ../..
 
+APP_STYPE="--onefile"
+if [ "$(uname)" == "Darwin" ]; then
+  echo "Building MacOS app"
+  APP_STYPE="--onedir"
+fi
+
+echo $APP_STYPE
+
 # Builds the desktop app
 # TODO: use a spec instead of long winded command line
-pyinstaller --windowed --onedir --icon="../icon.png" \
+pyinstaller --windowed $APP_STYLE --icon="../icon.png" \
   --add-data "../taskbar.png:." --add-data "../../studio/web_ui/build:./studio/web_ui/build" \
   -n fune --noconfirm --distpath=./desktop/build/dist --workpath=./desktop/build/work \
   --specpath=./desktop/build --osx-bundle-identifier=net.scosman.fune \
