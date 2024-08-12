f3eabed4bf10758600ae453ed749552b0dd49045
scosman <scosman@users.noreply.github.com>
2024-08-11 21:31:57 -0400

    Try taskbar only on windows

--- desktop/desktop.py
+++ desktop/desktop.py
@@ -7,6 +7,7 @@ import uvicorn
 import server.server as server
 import sys
 import threading
+import platform
 
 
 def resource_path(relative_path):
@@ -31,10 +32,12 @@ def show_studio():
 
 
 def quit_app():
+    # TODO: this is broken
     root.destroy()
+    icon.stop()
 
 
-def run_taskbar():
+def run_taskbar(detached=True):
     # TODO: resolution
     image = Image.open(resource_path("taskbar.png"))
     menu = (
@@ -42,13 +45,11 @@ def run_taskbar():
         pystray.MenuItem("Quit", quit_app),
     )
     icon = pystray.Icon("name", image, "title", menu)
-    # running detached since we use tk mainloop to get events from dock icon
-    icon.run_detached()
-
-
-def on_win_closing():
-    # Don't actually kill the app, just hide it
-    pass
+    if detached:
+        # running detached since we use tk mainloop to get events from dock icon
+        icon.run_detached()
+    else:
+        icon.run()
 
 
 def close_splash():
@@ -60,16 +61,30 @@ def close_splash():
         pass
 
 
-if __name__ == "__main__":
+def run_windows():
+    run_studio_thread()
+    show_studio()
+    close_splash()
+    run_taskbar(detached=False)
+
+
+# Extra work to get the dock icon to work on macOS
+def run_macos():
     # TK without a window, to get dock events
     root = tk.Tk()
+    root.withdraw()  # hide the window
     # Register callback for the dock icon to reopen the web app
     root.createcommand("tk::mac::ReopenApplication", show_studio)
-    root.protocol("WM_DELETE_WINDOW", on_win_closing)
-    root.withdraw()  # hide the window
-    run_taskbar()
+    run_taskbar(detached=True)
     # start the server in a thread, show the web app, and start the taskbar
     root.after(10, run_studio_thread)
     root.after(1000, show_studio)
     root.after(1000, close_splash)
     root.mainloop()
+
+
+if __name__ == "__main__":
+    if platform.system() == "Windows":
+        run_windows()
+    else:
+        run_macos()
