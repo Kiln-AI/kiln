eacd024249cf8c74789cd3e7c26f67eb8fe3742c
scosman <scosman@users.noreply.github.com>
2024-09-11 20:07:42 -0400

    Add a basic multi prompt adapter and tests

--- libs/core/kiln_ai/adapters/prompt_builders.py
+++ libs/core/kiln_ai/adapters/prompt_builders.py
@@ -20,3 +20,44 @@ class SimplePromptBuilder(BasePromptBuilder):
                 base_prompt += f"\n\n{adapter_instructions}"
 
         return base_prompt
+
+
+class MultiShotPromptBuilder(BasePromptBuilder):
+    def build_prompt(self) -> str:
+        base_prompt = f"# Instruction\n\n{ self.task.instruction }\n\n"
+
+        if len(self.task.requirements()) > 0:
+            base_prompt += "# Requirements\n\nYour response should respect the following requirements:\n"
+            for i, requirement in enumerate(self.task.requirements()):
+                base_prompt += f"{i+1}) {requirement.instruction}\n"
+            base_prompt += "\n"
+
+        # TODO: tons to do on selection here. This is just a quick version
+        valid_examples: list[tuple[str, str]] = []
+        for example in self.task.examples():
+            valid_output = None
+            for output in example.outputs():
+                if output.fixed_output is not None:
+                    valid_output = output.fixed_output
+                    break
+                elif output.rating is not None and output.rating.rating >= 4:
+                    valid_output = output.output
+                    break
+            if valid_output is not None:
+                valid_examples.append((example.input, valid_output))
+                if len(valid_examples) >= 10:
+                    break
+
+        if len(valid_examples) > 0:
+            base_prompt += "# Example Outputs\n\n"
+            for i, example in enumerate(valid_examples):
+                base_prompt += (
+                    f"## Example {i+1}\n\nInput: {example[0]}\nOutput: {example[1]}\n\n"
+                )
+
+        if self.adapter is not None:
+            adapter_instructions = self.adapter.adapter_specific_instructions()
+            if adapter_instructions is not None:
+                base_prompt += f"# Format Instructions\n\n{adapter_instructions}\n\n"
+
+        return base_prompt
--- libs/core/kiln_ai/adapters/test_prompt_builders.py
+++ libs/core/kiln_ai/adapters/test_prompt_builders.py
@@ -1,7 +1,17 @@
+import json
+
 from kiln_ai.adapters.base_adapter import BaseAdapter
-from kiln_ai.adapters.prompt_builders import SimplePromptBuilder
+from kiln_ai.adapters.prompt_builders import MultiShotPromptBuilder, SimplePromptBuilder
 from kiln_ai.adapters.test_prompt_adaptors import build_test_task
 from kiln_ai.adapters.test_structured_output import build_structured_output_test_task
+from kiln_ai.datamodel.models import (
+    Example,
+    ExampleOutput,
+    ExampleSource,
+    Project,
+    ReasonRating,
+    Task,
+)
 
 
 def test_simple_prompt_builder(tmp_path):
@@ -45,3 +55,80 @@ def test_simple_prompt_builder_structured_output(tmp_path):
     user_msg = builder.build_user_message(input)
     assert input in user_msg
     assert input not in prompt
+
+
+def test_multi_shot_prompt_builder(tmp_path):
+    # Create a project and task hierarchy
+    project = Project(name="Test Project", path=(tmp_path / "test_project.kiln"))
+    project.save_to_file()
+    task = Task(
+        name="Test Task",
+        instruction="You are an assistant which tells a joke, given a subject.",
+        parent=project,
+        input_json_schema=json.dumps(
+            {
+                "type": "object",
+                "properties": {
+                    "subject": {"type": "string"},
+                },
+                "required": ["subject"],
+            }
+        ),
+        output_json_schema=json.dumps(
+            {
+                "type": "object",
+                "properties": {"joke": {"type": "string"}},
+                "required": ["joke"],
+            }
+        ),
+    )
+    task.save_to_file()
+
+    # Create an example, but with no output
+    e1 = Example(
+        input='{"subject": "Cows"}',
+        source=ExampleSource.human,
+        parent=task,
+    )
+    e1.save_to_file()
+    check_example_outputs(task, 0)
+
+    # No review, so not valid
+    eo1 = ExampleOutput(
+        output='{"joke": "Moo I am a cow joke."}',
+        source=ExampleSource.human,
+        parent=e1,
+    )
+    eo1.save_to_file()
+    check_example_outputs(task, 0)
+
+    eo1.rating = ReasonRating(rating=4, reason="It's a good joke")
+    eo1.save_to_file()
+    check_example_outputs(task, 1)
+
+    e2 = Example(
+        input='{"subject": "Dogs"}',
+        source=ExampleSource.human,
+        parent=task,
+    )
+    e2.save_to_file()
+    eo2 = ExampleOutput(
+        output='{"joke": "This is a ruff joke."}',
+        source=ExampleSource.human,
+        parent=e2,
+        rating=ReasonRating(rating=4, reason="Bark"),
+    )
+    eo2.save_to_file()
+    check_example_outputs(task, 2)
+
+
+def check_example_outputs(task: Task, count: int):
+    prompt_builder = MultiShotPromptBuilder(task=task)
+    prompt = prompt_builder.build_prompt()
+    assert "# Instruction" in prompt
+    assert task.instruction in prompt
+    if count == 0:
+        assert "# Example Outputs" not in prompt
+    else:
+        assert "# Example Outputs" in prompt
+        assert f"## Example {count}" in prompt
