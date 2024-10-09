796425ba4a686fbbeca9e50115efec4ae46d2896
scosman <scosman@users.noreply.github.com>
2024-10-09 14:34:56 -0400

    New project button and chevrons

--- app/web_ui/src/routes/(app)/select_tasks_menu.svelte
+++ app/web_ui/src/routes/(app)/select_tasks_menu.svelte
@@ -76,9 +76,27 @@
   {#each project_list as project}
     {#if project.path == selected_project?.path}
       <li>
-        <h1>
-          <span class="badge badge-secondary badge-outline">Project</span>
-          {project.name}
+        <h1 class="flex flex-row pr-1">
+          <div class="grow">
+            <span class="badge badge-secondary badge-outline">Project</span>
+            {project.name}
+          </div>
+          <div>
+            <svg
+              fill="#000000"
+              class="w-3 h-3"
+              version="1.1"
+              id="Layer_1"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              viewBox="0 0 407.437 407.437"
+              xml:space="preserve"
+            >
+              <polygon
+                points="386.258,91.567 203.718,273.512 21.179,91.567 0,112.815 203.718,315.87 407.437,112.815 "
+              />
+            </svg>
+          </div>
         </h1>
         <ul>
           {#if tasks_loading}
@@ -109,11 +127,35 @@
       </li>
     {:else}
       <li>
-        <button on:click={() => select_project(project)}>
-          <span class="badge badge-secondary badge-outline">Project</span>
-          {project.name}
+        <button
+          on:click={() => select_project(project)}
+          class="flex flex-row pr-1"
+        >
+          <div class="grow">
+            <span class="badge badge-secondary badge-outline">Project</span>
+            {project.name}
+          </div>
+          <div>
+            <svg
+              fill="#000000"
+              class="w-3 h-3"
+              version="1.1"
+              id="Layer_1"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              viewBox="0 0 407.436 407.436"
+              xml:space="preserve"
+            >
+              <polygon
+                points="203.718,91.567 0,294.621 21.179,315.869 203.718,133.924 386.258,315.869 407.436,294.621 "
+              />
+            </svg>
+          </div>
         </button>
       </li>
     {/if}
   {/each}
+  <li class="pt-4">
+    <a href="/setup/create_project" class="font-medium">+ New Project</a>
+  </li>
 </ul>
